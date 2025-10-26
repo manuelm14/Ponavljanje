4d52f2950a2eef0d2f4698afc7fd5fd58e786f8e
manuelm14 <manuel.matosevic01@gmail.com>
2025-10-26 20:03:40 +0100

    funkcija za pozdrav.py

--- funkcija za pozdrav.py
+++ funkcija za pozdrav.py
@@ -1,7 +1,7 @@
 def kreiraj_pozdrav(ime):
     pozdravna_poruka=f"Pozdrav, {ime}!"
     return pozdravna_poruka
-pozdrav_za_anu=kreiraj_pozdrav("Ana")
+pozdrav_za_mariju=kreiraj_pozdrav("Marija")
 pozdrav_za_luku=kreiraj_pozdrav("Luka")
-print(pozdrav_za_anu)
+print(pozdrav_za_mariju)
 print(pozdrav_za_luku)
